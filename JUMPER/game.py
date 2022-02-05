cac3e5ee32b83b856fa38b879db64a81de513062
Brandon Ellis <brandonjosephellis@gmail.com>
2022-02-05 14:07:09 -0700

    game planning

--- JUMPER/game.py
+++ JUMPER/game.py
@@ -1,5 +1,28 @@
 
-
+import word as w
+from terminal_display import game_display, game_input
 
 class game:
-    pass
+    
+    def __init__(self):
+        self.guesses = []
+        self.chances = 5
+        self.display =  game_display() 
+        self.inputs = game_input()
+        self.word = w()
+        self.is_playing = True
+
+    def start_game(self):
+        #loops through game logic defined below
+        pass
+
+    def verify_guess(self):
+        #checks if the guess is correct or incorrect and appends score accoridingly 
+        pass
+
+    def check_gamestate(self):
+        #checks all the values in the game to see if the player has won or lost. 
+        pass
+    
+        
+    
