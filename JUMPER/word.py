586eb6e3f4f9c68fec3edd14286f943d20fcdd62
Brandon Ellis <brandonjosephellis@gmail.com>
2022-02-05 13:29:54 -0700

    word planning

--- JUMPER/word.py
+++ JUMPER/word.py
@@ -1,2 +1,17 @@
 class word:
-    pass
+    def __init__(self):
+        self.__master_list = []
+        self.__chosen_word = ""
+
+    def choose_word(self):
+        #select random word from internal list and save to chosen word
+        pass
+
+    def check_char(self,char):
+        #checks if passed character is in the chosen word
+        #returns bool
+        pass
+
+    def get_word(self):
+        #getter for the chosen word
+        pass
