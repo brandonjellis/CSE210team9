5b9c563d070e5978728c5d19a94295a382975082
Brandon Ellis <brandonjosephellis@gmail.com>
2022-02-12 18:57:12 -0700

    code refinement

--- JUMPER/game.py
+++ JUMPER/game.py
@@ -4,48 +4,52 @@ from terminal_display import game_display, game_input
 
 class game:
     
-    def __init__(self):
-        self.guesses = []
-        self.chances = 4
-        self.display =  game_display()
-        self.inputs = game_input()
-        self.word = w()
-        self.is_playing = True
+    def __init__(self, db = False):
+        self.debug = db
+        self._guesses = []
+        self._chances = 4
+        self._display = game_display()
+        self._inputs = game_input()
+        self._word = w()
+        self._is_playing = True
 
     def start_game(self):
         #loops through game logic defined below
 
-        self.word.choose_word()
-        #print(self.word.get_word()) #debug print
-        self.game_out()
+        self._word.choose_word()
+
+        if(self.debug):
+            print(self.word.get_word()) #debug print
+            
+        self.__game_out()
         while(self.is_playing):
-            self.game_in()
-            self.check_gamestate()
-            self.game_out()
+            self.__game_in()
+            self.__check_gamestate()
+            self.__game_out()
 
 
 
-    def game_in(self):
-        self.inputs.input(self.guesses)
-        self.guesses.append(self.inputs.get_input())
+    def __game_in(self):
+        self._inputs.input(self._guesses)
+        self._guesses.append(self._inputs.get_input())
 
-    def check_gamestate(self):
+    def __check_gamestate(self):
         #checks all the values in the game to see if the player has won or lost. 
-        if not(self.word.check_char(self.guesses[-1])):
-            self.chances -= 1
-        if self.chances <= 0:
+        if not(self._word.check_char(self._guesses[-1])):
+            self._chances -= 1
+        if self._chances <= 0:
             self.is_playing = False
-            self.display.print_defeat(self.word.get_word())
+            self._display.print_defeat(self._word.get_word())
         all_guessed = True
-        for c in self.word.get_word():
+        for c in self._word.get_word():
             if c not in self.guesses:
                 all_guessed = False
         if all_guessed:
             self.is_playing = False
-            self.display.print_win()
+            self._display.print_win()
 
-    def game_out(self):
-        self.display.draw_display(self.chances, self.guesses, self.word.get_word())
+    def __game_out(self):
+        self._display.draw_display(self._chances, self._guesses, self._word.get_word())
 
     
     
