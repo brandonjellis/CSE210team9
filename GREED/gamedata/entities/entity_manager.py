641b6e1c48a8584bad0d50d9a24af9b8d55ca8cc
Brandon Ellis <brandonjosephellis@gmail.com>
2022-02-19 18:06:41 -0700

    init 3.5

--- GREED/gamedata/entities/entity_manager.py
+++ GREED/gamedata/entities/entity_manager.py
@@ -1,4 +1,7 @@
 #organizes all the entities on screen and provides methods for managing them. 
 
+from entities.falling_objects import Object
+from entities.player import Player
+
 class Entity_Manager:
     pass
