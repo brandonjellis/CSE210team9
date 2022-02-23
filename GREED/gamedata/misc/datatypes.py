cca9721f2b6d31221192d44140e7f3949fb5f3de
Brandon Ellis <brandonjosephellis@gmail.com>
2022-02-23 14:37:20 -0700

    datatypes update 2/23

--- GREED/gamedata/misc/datatypes.py
+++ GREED/gamedata/misc/datatypes.py
@@ -23,6 +23,35 @@ class Vector:
     def set_z(self,z):
         self._zdir = z
 
+class point:
+    def __init__(self, x=0, y=0, z=0):
+        self._xp = x
+        self._yp = y
+        self._zp = z
+
+    def point_2d(self):
+        return (self._xp, self._yp)
+
+    def point_3d(self):
+        return (self._xp, self._yp, self._zp)
+
+    def set_point(self, x, y, z):
+        self._xp = x
+        self._yp = y
+        self._zp = z
+
+    def distance(self, target):
+        try:
+            t = target[2]
+        except:
+            t = 0
+        dx = self._xp - target[0]
+        dy = self._yp - target[1]
+        dz = self._zp - t
+        return math.sqrt(dx**2 + dy**2 + dz**2)
+
+
+
 
 class Color:
     def __init__(self, red = 255, green = 255, blue = 255, alpha = 255):
@@ -39,4 +68,3 @@ class Color:
 
     def get_color(self):
         return (self._red, self._blue, self._green, self._alpha)
-
