0a51eb6c91d8a4302d5fd1fcdd9e5a5dac96650f
Brandon Ellis <brandonjosephellis@gmail.com>
2022-02-23 15:13:26 -0700

    entity update 2/23 1

--- GREED/gamedata/entities/entity.py
+++ GREED/gamedata/entities/entity.py
@@ -11,8 +11,43 @@ class Entity:
         self._position = Point()
         self._velocity = Point()
     
-    def get_color(self):
-        return self._color
+    def get_icon(self):
+        return self._icon
 
     def get_size(self):
         return self._size
+
+    def get_color(self):
+        return self._color
+
+    def get_positon(self):
+        return self._position
+
+    def get_velocity(self):
+        return self._velocity
+
+    def set_icon(self, icon):
+        self._icon = icon
+
+    def set_size(self, size):
+        self._size = size 
+    
+    def set_color(self, color):
+        self._color = color
+
+    def set_position(self, pos):
+        self._position = pos 
+
+    def set_velocity(self, v):
+        self._velocity = v
+
+    def update_pos(self, max_x, max_y):
+        new_pos = self._position.point_2d()
+        new_pos[0] += self._velocity.point_2d()[0]
+        new_pos[1] += self._velocity.point_2d()[1]
+
+        new_pos[0] = 0 if new_pos[0] < 0 else max_x if new_pos[0] > max_x else new_pos[0]
+        new_pos[1] = 0 if new_pos[1] < 0 else max_y if new_pos[1] > max_y else new_pos[1]
+
+
+
