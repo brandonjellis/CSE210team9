0140ca760cd3befc37640581cdf233f47cc6e275
luiscardenas97 <97266653+luiscardenas97@users.noreply.github.com>
2022-03-19 17:43:40 -0600

    Luis Entity

--- FINAL_TOPGUN/gamedata/entities/entity.py
+++ FINAL_TOPGUN/gamedata/entities/entity.py
@@ -1,7 +1,38 @@
 
-
+import constants
+from gamedata.datatypes.color import Color
+from gamedata.datatypes.point import Point
+from gamedata.datatypes.animation import Animation
 
 class Entity:
     """
     contains basic parameters for anything on the screen.
     """
+    def __init__(self, position = Point(0,0), velocity = Point(0,0)): 
+        self._position = position
+        self._velocity = velocity
+        self._animation = Animation()
+
+    def get_position(self):
+        return self._position
+
+    def get_velocity(self):
+        return self._velocity
+
+    def get_animation(self):
+        return self._animation
+
+    def move_next(self):
+        x = (self._position.get_x() + self._velocity.get_x()) % constants.MAX_X
+        y = (self._position.get_x() + self._velocity.get_x()) % constants.MAX_Y 
+        self._position = Point(x,y)
+
+    def set_position(self, position):
+        self._position = position
+
+    def set_velocity(self, velocity):
+        self._velocity = velocity  
+
+    def set_animation(self, animation):
+        self._animation = animation
+        
