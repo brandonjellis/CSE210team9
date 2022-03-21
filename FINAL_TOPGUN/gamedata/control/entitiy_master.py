145928960b483afe16eacc1d54078336a480d6dc
luiscardenas97 <97266653+luiscardenas97@users.noreply.github.com>
2022-03-21 14:37:48 -0600

    luis

--- FINAL_TOPGUN/gamedata/control/entitiy_master.py
+++ FINAL_TOPGUN/gamedata/control/entitiy_master.py
@@ -1,9 +1,41 @@
 
-
-
 class EntityMaster:
     """
     provides a central source for managing all the entities in use.
     """
-    
-    pass
+    def __init__(self):
+        self._entities = {}
+
+    def add_entity(self, group, entity):
+        if not group in self._entities.keys():
+            self._entities[group] = []
+
+        if not entity in self._entities[group]:
+            self._entities[group] = []
+
+    def get_entities(self, group):
+        results = []
+        if group in self._entities.keys():
+            results = self._entities[group].copy()
+        return results
+
+    def get_all_entities(self):
+        results = []
+        for group in self._entities:
+            results.extend(self._entities[group])
+        return results
+
+    def get_first_entity(self, group):
+        result = None
+        if group in self._entities.keys():
+            result = self._actors[group][0]
+        return result
+
+    def remove_entity(self, group, entity):
+        if group in self._entities:
+            self._entities[group].remove(entity)
+
+    def remove_entities(self, group):
+        if group in self._entities:
+            for entity in self._entities[group]:
+                self._entities[group].remove(entity)
